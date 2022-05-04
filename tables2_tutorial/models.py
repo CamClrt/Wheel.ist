537e923d9517201d91b485e7703a6dad14dc1ecc
CamClrt <camille.clarret@gmail.com>
2022-05-04 06:33:24 +0200

    wip tables2_tutorial App

--- tables2_tutorial/models.py
+++ tables2_tutorial/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+
 class Person(models.Model):
     name = models.CharField(max_length=100, verbose_name="full name")
 
+    def __str__(self):
+        return self.name
